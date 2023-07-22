619512530517c89c6b49d7c51eca5963ad71f717
Massimo Hansen <massimo.hansen@gmail.com>
2023-07-22 16:33:06 +0200

    to pull

--- src/GameService/view/Player.js
+++ src/GameService/view/Player.js
@@ -1,9 +1,10 @@
 import React from "react";
 import TileService from "../TileService";
+import LeftRun from "../../images/character-left.png";
 
 const Player = params => {
     return <g id="player" transform={`translate(${TileService.tileSize * (params.startPos[0] + 0.5)}, ${TileService.tileSize * (params.startPos[1] + 0.5)})`}>
-        <circle background="red" r="20"/>
+        <image href={LeftRun} width={100} height={100}/>
     </g>
 }
 
