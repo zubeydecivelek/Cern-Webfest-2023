0890a43b11ef8260125f340b5ff106c6e8b89c40
Massimo Hansen <massimo.hansen@gmail.com>
2023-07-22 16:54:50 +0200

    added photos

--- src/GameService/view/Tile.js
+++ src/GameService/view/Tile.js
@@ -1,9 +1,13 @@
+import TileService from "../TileService";
 import "./Tile.css"
+import Walkable from "../../images/walk.png"
+import NonWalkable from "../../images/ground.png"
+
 
 const Tile = (params) => {
     console.log(params)
     return <g className="tile" transform={`translate(${params.x}, ${params.y})`}>
-        <rect width={params.size} height={params.size} className={params.walkable ? "floor" : "wall"}/>
+        <image href={params.walkable ? Walkable : NonWalkable} width={params.size} height={params.size} className={params.walkable ? "floor" : "wall"}/>
     </g>
 }
 
