ce1d8e5b00599533f4cd97821aa5fe8dec8b964b
Léa-Maria Rabour <leamaria.rabour@macbookrabour.dyndns.cern.ch>
2023-07-23 14:30:14 +0200

    changed text again

--- src/PopUpService/utils/informations.js
+++ src/PopUpService/utils/informations.js
@@ -9,8 +9,8 @@ const dialogLevel0 = {
 const dialogLevel1 = {
     "title" :  "Level 1 - Electrons and Protons",
     "informations" : [
-        "1899 : Joseph John Thomson (a british physicist) discovers the electron as the particles composing cathode rays, with a negative charge. He receives the Nobel prize in 1906 for that discovery.",
-        "1911 : Ernest Rutherford (british physicist as well) sends alpha particles (Helium nucleus, with a positive charge) on a foil of gold. This experiment shows that atoms are composed of a lot of void (lots of particles go through without scattering) and some particles are scattered/repulsed in the opposite direction, which indicates the presence of a positive charge inside the atom, since same sign charges repulse themselves while opposite charges get attracted.",
+        "1899 : Joseph John Thomson (a british physicist) discovers the electron as a particle with a negative charge. He receives the Nobel prize in 1906 for that discovery.",
+        "1911 : Ernest Rutherford (british physicist as well) sends alpha particles (Helium nucleus, with a positive charge) on a foil of gold. This experiment shows lots of particles going through without scattering which means that the atom is composed by a lot of void, and some particles being repulsed in the opposite direction, which indicates the presence of a positive charge inside the atom.",
         "The atom is now seen as a nucleus with positive charge (the protons) with negative charges  rotating around (the electrons). But part of the mass seems to be missing…"
     ]
 }
@@ -20,7 +20,8 @@ const dialogLevel2 = {
     "informations" : [
         "After Rutherford’s model of the atom, there was still one question that didnt get an answer “Why didnt the positively charged protons in the nucleus repel each other and cause the atom to be unstable?”",
         "To answer this question, James Chadwick, a British physicist, conducted a series of experiments. He observed a radiation with no charge that was able to penetrate matter more effectively than other known particles.",
-        "He concluded that this new radiation was composed of particles that were about the same mass as protons but carried no electric charge. Chadwick named these particles 'neutrons' due to their neutral nature, and received the Nobel prize in 1935 for this discovery."
+        "He concluded that this new radiation was composed of particles that were about the same mass as protons but carried no electric charge.",
+        "Chadwick named these particles 'neutrons' due to their neutral nature, and received the Nobel prize in 1935 for this discovery."
     ]
 }
 
@@ -28,10 +29,10 @@ const dialogLevel2 = {
 const dialogLevel3 = {
     "title" :  "Level 3 - Quarks and Gluons",
     "informations" : [
-        "In the mid-20th century, physicists observed new particles, similar to protons and neutrons but with other properties. American physicists Murray Gell-Mann and Israeli physicists Yuval Ne'eman independently proposed a way to organize these particles into groups based on their properties.",
-        "To explain the patterns observed in this classification, Gell-Mann and George Zweig, an American physicist, proposed that these particles (called hadrons) are made up of smaller particles called quarks, which come in different 'flavors': up, down, charm, strange, top and bottom.",
-        "This theory was supported by scattering experiments conducted by experimental physicists, indicating that these particles, including the protons and neutrons, were composed of quarks.",
-        "The quarks are sticked together inside of the protons and neutrons thank to the gluons, massless particles that mediate the strong force between quarks, analogous to how photons mediate the electromagnetic force between charged particles.",
+        "In the mid-20th century, physicists observed new particles, similar to protons and neutrons but with other properties.",
+        "American physicist Murray Gell-Mann and Israeli physicist Yuval Ne'eman independently proposed a way to organize these particles into groups based on their properties.",
+        "To explain the patterns observed in this classification, Gell-Mann and George Zweig, an American physicist, proposed that these particles, including protons and neutrons, are made up of smaller particles called quarks, which come in different 'flavors': up, down, charm, strange, top and bottom.",
+        "The quarks are glued together inside of the protons and neutrons thank to particles called gluons",
         "\n\nPhysics & Literature fact : The name quark comes from a line in James Joyce’s book Finnegans Wake : “Three quarks for Muster Mark!”"
     ]
 }
@@ -41,8 +42,9 @@ const dialogLevel4 = {
     "title" :  "Bonus level : Higgs",
     "informations" : [
         "Congratulations, you finished your journey through the subatomic world! But there is still a question unanswered… Where is the mass of all these particles coming from?",
-        "The particles gain their mass by interacting with the Higgs field. The stronger you interact with it, the more mass you have. The Higgs field is associated to the Higgs particle, whose existence was theorized by Robert Brout (belgian physicist with american origin), François Englert (belgian physicist) and Peter Higgs (british physicist). The Higgs particle was finally discovered at CERN in 2012, by ATLAS and CMS experiments in LHC. This was the missing piece of the Standard Model puzzle, but may not be the end of the story…",
-        "Woman in Science fact : Fabiola Gianotti was the spokesperson of ATLAS Experiment when the Higgs boson was discovered, and is now the Director General of CERN since 2016. A great example for young women who would like to pursue a career in science!",
+        "The particles gain their mass by interacting with the Higgs field. The Higgs field is a field of energy present everywhere in the universe. The stronger you interact with it, the more mass you have. ",
+        "The Higgs field is associated to the Higgs particle, whose existence was theorized by Robert Brout (belgian physicist with american origin), François Englert (belgian physicist) and Peter Higgs (british physicist). The Higgs particle was finally discovered at CERN in 2012, by ATLAS and CMS experiments in LHC. This was the missing piece of the Standard Model puzzle, but may not be the end of the story…",
+        "Women in Science fact : Fabiola Gianotti was the spokesperson of ATLAS Experiment when the Higgs boson was discovered, and is now the Director General of CERN since 2016. A great example for young women who would like to pursue a career in science!",
         "We are hoping to increase the number of female researchers and minorities in general winning Nobel prizes, because science is for everyone and every one of you could have a significant impact in the scientific community!"
     ]
 }
