8ac79f85ddfbb170250f6ef85982de50b6e19da6
Silia <siliataider@gmail.com>
2023-07-23 13:26:34 +0200

    Adding new tiles to the map

--- src/pages/constants.js
+++ src/pages/constants.js
@@ -1,5 +1,5 @@
-import characterImage1 from "../images2/apple.png"
-import characterImage2 from "../images2/character1.png"
+import characterImage1 from "../images/apple.png"
+import characterImage2 from "../images/character-W-left.png"
 import characterImage3 from "../images/character-left.png"
 
 export const characterImages = [characterImage1,characterImage2,characterImage3];
